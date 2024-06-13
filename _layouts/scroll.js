d59c94243d47372141e7bdd102acf44d636232b5
MovingJu <baramishe@gmail.com>
2024-06-13 09:02:28 +0900

    .

--- _layouts/scroll.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.horizontal-scroll').forEach(container => {
-    container.addEventListener('wheel', (e) => {
-      if (e.deltaY !== 0) {
-        e.preventDefault();
-        container.scrollLeft += e.deltaY;
-      }
-    });
-  });
-  
